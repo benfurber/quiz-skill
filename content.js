c22cb749482e1708cf9db9692d1abc5250ed6009
benfurber <ben@furber-consultancy.com>
2018-03-01 19:20:33 +0000

    Setup

---
+++ content.js
@@ -0,0 +1,18 @@
+module.exports = {
+  "Quiz 1": {
+    "Round 1": {
+      "Question 1" : {
+        "question": "Question 1 question?",
+        "answer": "Answer 1"
+      },
+      "Question 2" : {
+        "question": "Question 2?",
+        "answer": "Answer 2"
+      },
+      "Question 3" : {
+        "question": "A question words here?",
+        "answer": ""
+      },
+    },
+  }
+};

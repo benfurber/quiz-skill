e32b0eadd3d284981273a1305cdee4c5087be0f4
benfurber <ben@furber-consultancy.com>
2018-03-06 08:01:26 +0000

    Updated from lambda

--- testing/index.js
+++ testing/index.js
@@ -1,76 +1,276 @@
-var content = require('./quiz-content.js');
-var script = require('./quiz-script.js');
+/* This code has been generated from your interaction model
 
-var quizID = 1;
+/* eslint-disable  func-names */
+/* eslint quote-props: ["error", "consistent"]*/
 
-var progress = {
-  'round': 0,
-  'question': 0
+// There are three sections, Text Strings, Skill Code, and Helper Function(s).
+// You can copy and paste the contents as the code for a new Lambda function, using the alexa-skill-kit-sdk-factskill template.
+// This code includes helper functions for compatibility with versions of the SDK prior to 1.0.9, which includes the dialog directives.
+
+var content = require('./content.js');
+
+ // 1. Text strings =====================================================================================================
+ //    Modify these strings and messages to change the behavior of your Lambda function
+
+var speechOutput;
+var reprompt;
+var welcomeOutput = "This is a placeholder welcome message.";
+var welcomeReprompt = "sample re-prompt text";
+
+ // 2. Skill Code =======================================================================================================
+"use strict";
+var Alexa = require('alexa-sdk');
+var APP_ID = 'amzn1.ask.skill.71b5b8bd-87a5-48f4-a840-d467f28a92d5';
+var speechOutput = '';
+var handlers = {
+    'LaunchRequest': function () {
+          this.attributes['quizId'] = 1;
+          this.emit(':ask', welcomeOutput, welcomeReprompt);
+    },
+	'AMAZON.HelpIntent': function () {
+        speechOutput = '';
+        reprompt = '';
+        this.emit(':ask', speechOutput, reprompt);
+    },
+    'AMAZON.CancelIntent': function () {
+        speechOutput = '';
+        this.emit(':tell', speechOutput);
+    },
+    'AMAZON.StopIntent': function () {
+        speechOutput = '';
+        this.emit(':tell', speechOutput);
+    },
+    'SessionEndedRequest': function () {
+        speechOutput = '';
+        this.emit(':saveState', true); //uncomment to save attributes to db on session end
+        this.emit(':tell', speechOutput);
+    },
+	"AMAZON.NextIntent": function () {
+		var speechOutput = "";
+    	//any intent slot variables are listed here for convenience
+
+    	//Your custom intent handling goes here
+    	speechOutput = "This is a place holder response for the intent named AMAZON.NextIntent. This intent has no slots. Anything else?";
+        this.emit(":ask", speechOutput, speechOutput);
+    },
+	"AMAZON.RepeatIntent": function () {
+		var speechOutput = "";
+    	//any intent slot variables are listed here for convenience
+
+    	//Your custom intent handling goes here
+    	speechOutput = "This is a place holder response for the intent named AMAZON.RepeatIntent. This intent has no slots. Anything else?";
+        this.emit(":ask", speechOutput, speechOutput);
+    },
+	"AMAZON.StartOverIntent": function () {
+		var speechOutput = "";
+    	//any intent slot variables are listed here for convenience
+
+    	//Your custom intent handling goes here
+    	speechOutput = "This is a place holder response for the intent named AMAZON.StartOverIntent. This intent has no slots. Anything else?";
+        this.emit(":ask", speechOutput, speechOutput);
+    },
+	"StartQuizIntent": function () {
+		var speechOutput = "";
+    	//any intent slot variables are listed here for convenience
+
+    	//Your custom intent handling goes here
+    	speechOutput = "This is a place holder response for the intent named StartQuizIntent. This intent has no slots. Anything else?";
+        this.emit(":ask", speechOutput, speechOutput);
+    },
+	'Unhandled': function () {
+        speechOutput = "The skill didn't quite understand what you wanted.  Do you want to try something else?";
+        this.emit(':ask', speechOutput, speechOutput);
+    }
 };
 
-function progressController() {
+exports.handler = (event, context) => {
+    var alexa = Alexa.handler(event, context);
+    alexa.appId = APP_ID;
+    // To enable string internationalization (i18n) features, set a resources object.
+    //alexa.resources = languageStrings;
+    alexa.registerHandlers(handlers);
+	  alexa.dynamoDBTableName = 'QuizSkill';
+    alexa.execute();
+};
 
-  var text = [];
+//    END of Intent Handlers {} ========================================================================================
+// 3. Helper Function  =================================================================================================
 
-  // Work out where the user is
+function resolveCanonical(slot){
+	//this function looks at the entity resolution part of request and returns the slot value if a synonyms is provided
+    try{
+		var canonical = slot.resolutions.resolutionsPerAuthority[0].values[0].value.name;
+	}catch(err){
+	    console.log(err.message);
+	    var canonical = slot.value;
+	};
+	return canonical;
+};
 
-    // The user has just starting of the quiz
-    if (progress['round'] == 0) {
-      text.push(script['quizIntro']);
-      text.push(content[quizID]['Description']);
-      progress['round']++;
+function delegateSlotCollection(){
+  console.log("in delegateSlotCollection");
+  console.log("current dialogState: "+this.event.request.dialogState);
+    if (this.event.request.dialogState === "STARTED") {
+      console.log("in Beginning");
+	  var updatedIntent= null;
+	  // updatedIntent=this.event.request.intent;
+      //optionally pre-fill slots: update the intent object with slot values for which
+      //you have defaults, then return Dialog.Delegate with this updated intent
+      // in the updatedIntent property
+      //this.emit(":delegate", updatedIntent); //uncomment this is using ASK SDK 1.0.9 or newer
+
+	  //this code is necessary if using ASK SDK versions prior to 1.0.9
+	  if(this.isOverridden()) {
+			return;
+		}
+		this.handler.response = buildSpeechletResponse({
+			sessionAttributes: this.attributes,
+			directives: getDialogDirectives('Dialog.Delegate', updatedIntent, null),
+			shouldEndSession: false
+		});
+		this.emit(':responseReady', updatedIntent);
+
+    } else if (this.event.request.dialogState !== "COMPLETED") {
+      console.log("in not completed");
+      // return a Dialog.Delegate directive with no updatedIntent property.
+      //this.emit(":delegate"); //uncomment this is using ASK SDK 1.0.9 or newer
+
+	  //this code necessary is using ASK SDK versions prior to 1.0.9
+		if(this.isOverridden()) {
+			return;
+		}
+		this.handler.response = buildSpeechletResponse({
+			sessionAttributes: this.attributes,
+			directives: getDialogDirectives('Dialog.Delegate', updatedIntent, null),
+			shouldEndSession: false
+		});
+		this.emit(':responseReady');
+
+    } else {
+      console.log("in completed");
+      console.log("returning: "+ JSON.stringify(this.event.request.intent));
+      // Dialog is now complete and all required slots should be filled,
+      // so call your normal intent handler.
+      return this.event.request.intent;
+    }
+}
+
+
+function randomPhrase(array) {
+    // the argument is an array [] of words or phrases
+    var i = 0;
+    i = Math.floor(Math.random() * array.length);
+    return(array[i]);
+}
+function isSlotValid(request, slotName){
+        var slot = request.intent.slots[slotName];
+        //console.log("request = "+JSON.stringify(request)); //uncomment if you want to see the request
+        var slotValue;
+
+        //if we have a slot, get the text and store it into speechOutput
+        if (slot && slot.value) {
+            //we have a value in the slot
+            slotValue = slot.value.toLowerCase();
+            return slotValue;
+        } else {
+            //we didn't get a value in the slot.
+            return false;
+        }
+}
+
+//These functions are here to allow dialog directives to work with SDK versions prior to 1.0.9
+//will be removed once Lambda templates are updated with the latest SDK
+
+function createSpeechObject(optionsParam) {
+    if (optionsParam && optionsParam.type === 'SSML') {
+        return {
+            type: optionsParam.type,
+            ssml: optionsParam['speech']
+        };
+    } else {
+        return {
+            type: optionsParam.type || 'PlainText',
+            text: optionsParam['speech'] || optionsParam
+        };
+    }
+}
+
+function buildSpeechletResponse(options) {
+    var alexaResponse = {
+        shouldEndSession: options.shouldEndSession
+    };
+
+    if (options.output) {
+        alexaResponse.outputSpeech = createSpeechObject(options.output);
     }
 
-    // The user is about to start a new round
-    if (progress['round'] > 0 && progress['question'] == 0) {
-      text.push(script['startOfRound']);
-      text.push(content[quizID][progress['round']]['Description']);
-      progress['question']++;
+    if (options.reprompt) {
+        alexaResponse.reprompt = {
+            outputSpeech: createSpeechObject(options.reprompt)
+        };
+    }
+
+    if (options.directives) {
+        alexaResponse.directives = options.directives;
+    }
+
+    if (options.cardTitle && options.cardContent) {
+        alexaResponse.card = {
+            type: 'Simple',
+            title: options.cardTitle,
+            content: options.cardContent
+        };
+
+        if(options.cardImage && (options.cardImage.smallImageUrl || options.cardImage.largeImageUrl)) {
+            alexaResponse.card.type = 'Standard';
+            alexaResponse.card['image'] = {};
+
+            delete alexaResponse.card.content;
+            alexaResponse.card.text = options.cardContent;
+
+            if(options.cardImage.smallImageUrl) {
+                alexaResponse.card.image['smallImageUrl'] = options.cardImage.smallImageUrl;
+            }
+
+            if(options.cardImage.largeImageUrl) {
+                alexaResponse.card.image['largeImageUrl'] = options.cardImage.largeImageUrl;
+            }
+        }
+    } else if (options.cardType === 'LinkAccount') {
+        alexaResponse.card = {
+            type: 'LinkAccount'
+        };
+    } else if (options.cardType === 'AskForPermissionsConsent') {
+        alexaResponse.card = {
+            type: 'AskForPermissionsConsent',
+            permissions: options.permissions
+        };
+    }
+
+    var returnResult = {
+        version: '1.0',
+        response: alexaResponse
     };
 
-    // The user needs a question
-    if (progress['round'] > 0 && progress['question'] > 0) {
+    if (options.sessionAttributes) {
+        returnResult.sessionAttributes = options.sessionAttributes;
+    }
+    return returnResult;
+}
 
-      // Finding out the total number of questions in the round
-      var questionsInRound = content[quizID][progress['round']]['Total'];
-
-      // The user will finish the round on this question
-      if (progress['question'] == questionsInRound) {
-        text.push(script['endofRound']);
-      };
-
-      // Say what question the user is on
-      text.push('Question ' + progress['question']);
-
-      // Say the question
-      text.push(content[quizID][progress['round']][progress['question']]['question']);
-
-      // Progress addition
-      if (progress['question'] == questionsInRound) {
-
-        // Progress to the next round
-        progress['round']++;
-        // Reset the question count
-        progress['question'] = 0
-
-      } else {
-        progress['question']++;
-      };
-
+function getDialogDirectives(dialogType, updatedIntent, slotName) {
+    let directive = {
+        type: dialogType
     };
 
-  // Action
+    if (dialogType === 'Dialog.ElicitSlot') {
+        directive.slotToElicit = slotName;
+    } else if (dialogType === 'Dialog.ConfirmSlot') {
+        directive.slotToConfirm = slotName;
+    }
 
-    // Call the text
-    console.log(text.join(' \n'));
-    console.log('\r');
-
-};
-
-progressController()
-progressController()
-progressController()
-progressController()
-progressController()
-progressController()
-progressController()
+    if (updatedIntent) {
+        directive.updatedIntent = updatedIntent;
+    }
+    return [directive];
+}
